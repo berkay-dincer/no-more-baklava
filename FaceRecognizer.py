3e3992cc8a83497721356c3d2a603a5c2f26e0bd
Berkay Dincer <berkayd@scotty.com.tr>
2019-01-30 18:02:55 +0300

    refactores code, now more reliable without face recognition

--- FaceRecognizer.py
+++ FaceRecognizer.py
@@ -1,14 +1,6 @@
-import pickle
 import signal
-import os
-
+import datetime
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-from keras.models import load_model
-from skimage.transform import resize
-from sklearn.preprocessing import LabelEncoder
-from sklearn.svm import SVC
 
 
 class FaceRecognize(object):
@@ -19,137 +11,11 @@ class FaceRecognize(object):
         self.batch_size = 1
         self.is_interrupted = False
         self.data = {}
-        self.le = None
-        self.clf = None
-        self.model = load_model('model/facenet_keras.h5')
-
-    def _prewhiten(self, x):
-        if x.ndim == 4:
-            axis = (1, 2, 3)
-            size = x[0].size
-        elif x.ndim == 3:
-            axis = (0, 1, 2)
-            size = x.size
-        else:
-            raise ValueError('Dimension should be 3 or 4')
-
-        mean = np.mean(x, axis=axis, keepdims=True)
-        std = np.std(x, axis=axis, keepdims=True)
-        std_adj = np.maximum(std, 1.0 / np.sqrt(size))
-        y = (x - mean) / std_adj
-        return y
-
-    def _l2_normalize(self, x, axis=-1, epsilon=1e-10):
-        output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
-        return output
-
-    def _calc_embs(self, imgs, margin, batch_size):
-        aligned_images = self._prewhiten(imgs)
-        pd = []
-        for start in range(0, len(aligned_images), batch_size):
-            pd.append(self.model.predict_on_batch(aligned_images[start:start + batch_size]))
-        embs = self._l2_normalize(np.concatenate(pd))
-
-        return embs
 
     def _signal_handler(self, signal, frame):
         self.is_interrupted = True
 
-    def _load_training_pickles(self):
-        pickle_dir = 'owner_images/'
-        pickles = os.listdir(pickle_dir)
-        all_data = dict()
-        for p in pickles:
-            pickle_in = open(pickle_dir + "/" + p, "rb")
-            face_data = pickle.load(pickle_in)
-            all_data = {**all_data, **face_data}
-        self.data = all_data
-
-    def capture_images(self, image_count, name='Unknown'):
-        vc = cv2.VideoCapture(0)
-        self.vc = vc
-        if vc.isOpened():
-            is_capturing, _ = vc.read()
-        else:
-            is_capturing = False
-
-        imgs = []
-        signal.signal(signal.SIGINT, self._signal_handler)
-        self.is_interrupted = False
-        while is_capturing:
-            is_capturing, frame = vc.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            faces = self.cascade.detectMultiScale(frame,
-                                                  scaleFactor=1.1,
-                                                  minNeighbors=3,
-                                                  minSize=(100, 100))
-            if len(faces) != 0:
-                face = faces[0]
-                (x, y, w, h) = face
-                left = x - self.margin // 2
-                right = x + w + self.margin // 2
-                bottom = y - self.margin // 2
-                top = y + h + self.margin // 2
-                img = resize(frame[bottom:top, left:right, :],
-                             (160, 160), mode='reflect')
-                imgs.append(img)
-                cv2.rectangle(frame,
-                              (left - 1, bottom - 1),
-                              (right + 1, top + 1),
-                              (255, 0, 0), thickness=2)
-
-            plt.imshow(frame)
-            plt.title('{}/{}'.format(len(imgs), image_count))
-            plt.xticks([])
-            plt.yticks([])
-            if len(imgs) == image_count:
-                vc.release()
-                self.data[name] = np.array(imgs)
-                pickle_out = open("owner_images/" + name + ".pickle", "wb")
-                pickle.dump(self.data, pickle_out)
-                pickle_out.close()
-                break
-            try:
-                plt.pause(0.1)
-            except Exception:
-                pass
-            if self.is_interrupted:
-                vc.release()
-                break
-
-    def train(self):
-        self._load_training_pickles()
-        labels = []
-        embs = []
-        self._load_training_pickles()
-        names = self.data.keys()
-        for name, imgs in self.data.items():
-            embs_ = self._calc_embs(imgs, self.margin, self.batch_size)
-            labels.extend([name] * len(embs_))
-            embs.append(embs_)
-
-        embs = np.concatenate(embs)
-        le = LabelEncoder().fit(labels)
-        y = le.transform(labels)
-        clf = SVC(kernel='linear', probability=True).fit(embs, y)
-
-        self.le = le
-        self.clf = clf
-        pickle_le = open("le.pickle", "wb")
-        pickle.dump(le, pickle_le)
-        pickle_le.close()
-
-        pickle_clf = open("clf.pickle", "wb")
-        pickle.dump(clf, pickle_clf)
-        pickle_clf.close()
-
-    def infer(self):
-        pickle_le = open("le.pickle", "rb")
-        self.le = pickle.load(pickle_le)
-
-        pickle_clf = open("clf.pickle", "rb")
-        self.clf = pickle.load(pickle_clf)
-
+    def is_face_present(self):
         vc = cv2.VideoCapture(0)
         self.vc = vc
         if vc.isOpened():
@@ -166,35 +32,15 @@ class FaceRecognize(object):
                                                   scaleFactor=1.1,
                                                   minNeighbors=3,
                                                   minSize=(100, 100))
-            pred = None
             if len(faces) != 0:
-                face = faces[0]
-                (x, y, w, h) = face
-                left = x - self.margin // 2
-                right = x + w + self.margin // 2
-                bottom = y - self.margin // 2
-                top = y + h + self.margin // 2
-                img = resize(frame[bottom:top, left:right, :],
-                             (160, 160), mode='reflect')
-                embs = self._calc_embs(img[np.newaxis], self.margin, 1)
-                pred = self.le.inverse_transform(self.clf.predict(embs))
-                cv2.rectangle(frame,
-                              (left - 1, bottom - 1),
-                              (right + 1, top + 1),
-                              (255, 0, 0), thickness=2)
-            plt.imshow(frame)
-            plt.title(pred)
-            plt.xticks([])
-            plt.yticks([])
-            try:
-                plt.pause(0.1)
-            except Exception:
-                pass
-            if self.is_interrupted:
-                vc.release()
-                break
-            return pred
+                current_datetime = datetime.datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
+                image_name = 'images/' + current_datetime + '.jpg'
+                cv2.imwrite(image_name, frame)
+                print("!!!! Saved image at {} find out who was trying to order baklava!".format(image_name))
+                return True
+            return False
 
     def close_camera(self):
         vc = cv2.VideoCapture(0)
         vc.release()
+
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import argparse
 import subprocess
 import time
 from ctypes import CDLL
+import datetime
+
 from FaceRecognizer import FaceRecognize
 
 
-def idleTime():
-    '''Return idle time in seconds'''
+def get_readable_datetime():
+    return datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
 
-    # Get the output from
-    # ioreg -c IOHIDSystem
-    # /usr/sbin/ioreg -c IOHIDSystem | /usr/bin/awk '/HIDIdleTime/ {print int($NF/1000000000)}'
+
+def get_idle_time():
     io_stats = subprocess.Popen(["ioreg", "-c", "IOHIDSystem"], stdout=subprocess.PIPE)
     result = subprocess.Popen(["grep", "HIDIdleTime"], stdin=io_stats.stdout, stdout=subprocess.PIPE)
 
@@ -34,28 +34,20 @@ def lock_screen():
     CDLL('/System/Library/PrivateFrameworks/login.framework/Versions/Current/login').SACLockScreenImmediate()
 
 
-def main(mode, image_count, name):
+def main():
     recognizer = FaceRecognize()
-    if mode == 'train':
-        recognizer.capture_images(name=name, image_count=image_count)
-        recognizer.train()
-    else:
-        while True:
-            time.sleep(1)
-            print("Baklava check...")
-            if idleTime() > 10:
-                print("Computer is idle we will not eat baklava today..")
-                detected_face = recognizer.infer()[0]
-                print(detected_face)
-                if detected_face not in name:
-                    lock_screen()
+    while True:
+        time.sleep(2)
+        current_idle_time = get_idle_time()
+        print("{} - Baklava ping. Current idle time is: {}".format(get_readable_datetime(), current_idle_time))
+        if current_idle_time > 20:
+            if recognizer.is_face_present():
+                print("{} - Detected a face. Locking computer, we will not eat baklava today..".format(
+                    get_readable_datetime()))
+                lock_screen()
                 recognizer.close_camera()
+                break
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Add some integers.')
-    parser.add_argument('mode', type=str, help='mode of execution - train, no-more-baklava')
-    parser.add_argument('count', type=int, default=10, help='number of pictures to take for training session.')
-    parser.add_argument('name', type=str, help='name of the person in picture.')
-    args = parser.parse_args()
-    main(args.mode, args.count, args.name)
+    main()
